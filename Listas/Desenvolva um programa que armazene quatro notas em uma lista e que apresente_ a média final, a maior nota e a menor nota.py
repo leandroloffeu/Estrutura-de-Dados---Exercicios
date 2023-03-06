89ab3ddd11a62a41ce924c646a9ab094a5187257
leandroloffeu <112645165+leandroloffeu@users.noreply.github.com>
2023-03-06 20:28:15 -0300

    Atualizar o Desenvolva um programa que armazene quatro notas em uma lista e que apresente_ a média final, a maior nota e a menor nota.py

--- Listas/Desenvolva um programa que armazene quatro notas em uma lista e que apresente_ a média final, a maior nota e a menor nota.py
+++ Listas/Desenvolva um programa que armazene quatro notas em uma lista e que apresente_ a média final, a maior nota e a menor nota.py
@@ -1,6 +1,6 @@
 #Desenvolva um programa que armazene quatro notas em uma lista e que apresente: a média final, a maior nota e a menor nota
 
-notas = []  # cria uma lista vazia para armazenar as notas
+notas = []
 
 nota1 = float (input(f'informe a 1º nota: '))
 notas.append(nota1)
@@ -11,16 +11,16 @@ notas.append(nota3)
 nota4 = float (input(f'informe a 4º nota: '))
 notas.append(nota4)
 
-# calcula a média final
+
 media = sum(notas) / len(notas)
 
-# encontra a maior nota
+
 maior_nota = max(notas)
 
-# encontra a menor nota
+
 menor_nota = min(notas)
 
-# apresenta os resultados
+
 print(f"Média final: {media:.2f}")
 print(f"Maior nota: {maior_nota}")
 print(f"Menor nota: {menor_nota}")
