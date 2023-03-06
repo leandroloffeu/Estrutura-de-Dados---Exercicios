63d3502e3dede253dbe338dc712c61fbf9213b0a
leandroloffeu <112645165+leandroloffeu@users.noreply.github.com>
2023-03-06 18:58:13 -0300

    Add files via upload

---
+++ Listas/Desenvolva um programa que pergunte o seu nome e, ao teclar Enter, apresente uma saudação personalizada com seu nome.py
@@ -0,0 +1,5 @@
+#Desenvolva um programa que pergunte o seu nome e, ao teclar Enter, apresente uma saudação personalizada com seu nome.
+
+nome = input (str("Qual é o seu nome?" ))  # pede ao usuário para inserir o seu nome
+print("Olá, " + nome + " Tudo bem com você?")  # apresenta uma saudação personalizada com o nome do usuário
+print("caro" + nome + "informe credenciais")
